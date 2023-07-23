b7f99056892d8773f91a4f9b682a582556f0d0b0
Suresh Kumar P <suresh.palani@abjayon.com>
2023-07-23 23:51:20 +0530

    my first sample nestjs bookmark project

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return { msg: 'I am signed up' };
+  }
+
+  signin() {
+    return { msg: 'I am signin in' };
+  }
+}
